c0dbd957d72d2121238392ea1cb68c3573495909
Tushar Tomar <tomartushar5@gmail.com>
2022-05-02 14:45:12 +0530

    Fixed Sidebar Error

--- online-learn/src/components/Home.js
+++ online-learn/src/components/Home.js
@@ -1,18 +1,11 @@
 import React, { useState } from "react";
-import logo from "../Assests/icons8-bbb.svg";
 import Course1_img from "../Assests/Course1_img.svg";
 import Course2_img from "../Assests/Course2_img.svg";
-import MenuBookIcon from "@mui/icons-material/MenuBook";
-import LogoutIcon from "@mui/icons-material/Logout";
-import DiamondIcon from "@mui/icons-material/Diamond";
-import PeopleIcon from "@mui/icons-material/People";
+
 import StarIcon from "@mui/icons-material/Star";
 import PlayCircleOutlineIcon from "@mui/icons-material/PlayCircleOutline";
 import { NavLink, useNavigate } from "react-router-dom";
-
-const isNotActiveStyle = "text-[#555555] flex flex-col items-center";
-const isActiveStyle =
-  "border-r-2 border-white  text-white flex flex-col items-center";
+import HomeSidebar from "./HomeSidebar";
 
 const sampleDate = [
   {
@@ -101,9 +94,7 @@ var colors = ["#C87343", "#DFB23C", "#4F8834", "#345688", "#813488"];
 
 const Home = () => {
   const navigate = useNavigate();
-  const logout = () => {
-    navigate("/login/adminLogin");
-  };
+
   const [totalStars, setTotalStars] = useState([]);
   const [openCourse, setOpenCourse] = useState(sampleDate[0]);
   const calculateTotalStars = (stars) => {
@@ -118,39 +109,7 @@ const Home = () => {
 
   return (
     <div className="bg-black w-screen h-screen flex overflow-hidden">
-      <div className="h-[45.5rem] flex-[0.07] flex flex-col items-center my-4 justify-between py-5">
-        <img src={logo} alt="" />
-        <div className="flex flex-col space-y-10">
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              isActive ? isActiveStyle : isNotActiveStyle
-            }>
-            <MenuBookIcon className=" h-7" alt="" />
-            <p className="">Courses</p>
-          </NavLink>
-          <NavLink
-            to="/resources"
-            className={({ isActive }) =>
-              isActive ? isActiveStyle : isNotActiveStyle
-            }>
-            <DiamondIcon className=" h-7" alt="" />
-            <p className="">Resources</p>
-          </NavLink>
-          <NavLink
-            to="/community"
-            className={({ isActive }) =>
-              isActive ? isActiveStyle : isNotActiveStyle
-            }>
-            <PeopleIcon className=" h-7" alt="" />
-            <p className="">Community</p>
-          </NavLink>
-        </div>
-        <div className="flex flex-col items-center space-y-2 text-[#555555] hover:text-white hover:scale-110 transition-all duration-150">
-          <LogoutIcon onClick={logout} className="cursor-pointer h-10  " />
-          <p className="">Logout</p>
-        </div>
-      </div>
+      <HomeSidebar />
       <div className="h-[45.5rem] bg-white flex-[0.93] my-4 rounded-2xl mr-4 flex">
         <div className="flex flex-col flex-[0.45] bg-[#F9F9F9] rounded-tl-2xl rounded-bl-2xl  pt-[53px]">
           <h1 className="font-bold text-[40px] px-[52px]">Courses</h1>
