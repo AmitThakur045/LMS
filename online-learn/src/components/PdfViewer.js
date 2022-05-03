f73f473f872ba536646214a6508e3fbf8fc00059
Amit Thakur <at7129652@gmail.com>
2022-05-03 11:06:58 +0530

    added download button

--- online-learn/src/components/PdfViewer.js
+++ online-learn/src/components/PdfViewer.js
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
+import { saveAs } from "file-saver";
+import { Button } from "@mui/material";
+import DownloadForOfflineIcon from '@mui/icons-material/DownloadForOffline';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 export default function PdfViewer(props) {
+  const { pdf } = props;
   const [state, setState] = useState({
     numPages: 0,
   });
@@ -11,10 +15,20 @@ export default function PdfViewer(props) {
     setState(numPages);
   }
 
-  const { pdf } = props;
+  const saveFile = () => {
+    saveAs(pdf, "example.pdf");
+  };
 
   return (
     <div className="pl-32">
+      <div style={{ position: "fixed", top: "116px", right: "50px", "z-index": "1"}}>
+        <a href={pdf} onClick={saveFile} download>
+          <DownloadForOfflineIcon
+            style={{ fontSize: "3.5rem"}}
+            color="action"
+          />
+        </a>
+      </div>
       <Document
         file={pdf}
         options={{ workerSrc: "/pdf.worker.js" }}
