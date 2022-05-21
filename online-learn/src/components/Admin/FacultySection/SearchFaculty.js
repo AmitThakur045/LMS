b8714c5e5328eae48e86e0b2918d3b4237c897b3
Tushar Tomar <tomartushar5@gmail.com>
2022-05-21 16:38:51 +0530

    Added New UI Course and Student Section

--- online-learn/src/components/Admin/FacultySection/SearchFaculty.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const SearchFaculty = () => {
-  return <div>SearchFaculty</div>;
-};
-
-export default SearchFaculty;
--- online-learn/src/components/Admin/StudentSection/ViewStudent/Main.js
+++ online-learn/src/components/Admin/StudentSection/ViewStudent/Main.js
@@ -8,37 +8,50 @@ import ActiveBatch from "../../ActiveBatch";
 import RecentNotification from "../../RecentNotification";
 
 import {
+  Checkbox,
   FormControl,
   InputLabel,
+  ListItemText,
   MenuItem,
+  OutlinedInput,
   Select,
   TextField,
 } from "@mui/material";
 import Spinner from "../../../../Utils/Spinner";
 import { useNavigate } from "react-router-dom";
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
 
 const Main = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState({});
   const dispatch = useDispatch();
-  const admin = useSelector((store) => store.admin.admin);
+  const student = useSelector((store) => store.admin.student);
   const navigate = useNavigate();
   useEffect(() => {
     dispatch({ type: SET_ERRORS, payload: {} });
-    if (Object.keys(admin).length === 0) navigate("/admin/admin");
+    if (Object.keys(student).length === 0) navigate("/admin/student");
   }, []);
 
   return (
     <div className="flex overflow-hidden h-full space-x-5 px-12 mb-5">
       <div className="w-[80%] rounded-3xl bg-[#FAFBFF] px-10 py-5 flex flex-col space-y-4">
-        <p className="text-[#8d91b1]">View Admin</p>
+        <p className="text-[#8d91b1]">View Student</p>
         <div className="flex space-x-16">
           <div className="w-[40%] flex items-center justify-center">
             <div className="w-[250px] h-[227px] bg-white border-[1px] border-[#CBCBCB] flex flex-col items-center justify-center">
               <img
-                src={admin.avatar}
+                src={student.avatar}
                 className="w-full h-full object-cover"
-                alt="Profile"
+                alt=""
               />
             </div>
           </div>
@@ -51,7 +64,7 @@ const Main = () => {
                 label="First Name"
                 variant="outlined"
                 className="bg-white"
-                value={admin.firstName}
+                value={student.firstName}
               />
               <TextField
                 aria-disabled
@@ -60,29 +73,29 @@ const Main = () => {
                 label="Last Name"
                 variant="outlined"
                 className="bg-white"
-                value={admin.lastName}
+                value={student.lastName}
               />
             </div>
-            <div className="flex">
+            <div className="flex space-x-8">
               <TextField
                 aria-disabled
                 type="email"
                 id="outlined-basic"
                 label="Email"
                 variant="outlined"
-                className="bg-white w-full"
-                value={admin.email}
+                className="bg-white"
+                value={student.email}
               />
-            </div>
-            <div className="flex justify-between space-x-8">
               <TextField
                 aria-disabled
                 type="date"
                 id="outlined-basic"
                 variant="outlined"
-                className="bg-white w-[40%]"
-                value={admin.dob}
+                className="bg-white "
+                value={student.dob}
               />
+            </div>
+            <div className="flex space-x-8 ">
               <TextField
                 aria-disabled
                 type="number"
@@ -90,51 +103,42 @@ const Main = () => {
                 label="Contact Number"
                 variant="outlined"
                 className="bg-white w-[60%]"
-                value={admin.contactNumber}
+                value={student.contactNumber}
+              />
+              <TextField
+                aria-disabled
+                type="text"
+                id="outlined-basic"
+                label="Performance"
+                variant="outlined"
+                className="bg-white w-[60%]"
+                value={student.performance}
               />
             </div>
-            <div className="flex justify-between ">
-              <FormControl aria-disabled className="w-[47%]">
-                <InputLabel id="demo-simple-select-label">Sub Admin</InputLabel>
+            <div className="flex">
+              <FormControl sx={{ width: 475 }}>
+                <InputLabel id="demo-multiple-checkbox-label">Batch</InputLabel>
                 <Select
-                  labelId="demo-simple-select-label"
-                  id="demo-simple-select"
-                  value={admin.sub}
-                  label="Sub Admin">
-                  <MenuItem value="true">Yes</MenuItem>
-                  <MenuItem value="false">No</MenuItem>
+                  aria-disabled
+                  labelId="demo-multiple-checkbox-label"
+                  id="demo-multiple-checkbox"
+                  multiple
+                  value={student.batch}
+                  input={<OutlinedInput label="Batch" />}
+                  renderValue={(selected) => selected.join(", ")}
+                  MenuProps={MenuProps}>
+                  {student.batch?.map((bt) => (
+                    <MenuItem key={bt} value={bt}>
+                      <Checkbox checked={student.batch.indexOf(bt) > -1} />
+                      <ListItemText primary={bt} />
+                    </MenuItem>
+                  ))}
                 </Select>
               </FormControl>
-              {admin.sub === "true" && (
-                <FormControl aria-disabled className="w-[47%]">
-                  <InputLabel id="demo-simple-select-label">Domain</InputLabel>
-                  <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={admin.domain}
-                    label="Domain">
-                    <MenuItem value="AI">AI</MenuItem>
-                    <MenuItem value="Data Science">Data Science</MenuItem>
-                    <MenuItem value="Java Development">
-                      Java Development
-                    </MenuItem>
-                    <MenuItem value="Web Development">Web Development</MenuItem>
-                    <MenuItem value="Machine Learning">
-                      Machine Learning
-                    </MenuItem>
-                  </Select>
-                </FormControl>
-              )}
             </div>
           </div>
         </div>
-
-        {loading && <Spinner message="Adding Admin" />}
-        {error.emailError && (
-          <p className="text-red-500 flex self-center">{error.emailError}</p>
-        )}
       </div>
-
       <div className="bg-[#FAFBFF] w-[20%] flex flex-col px-5 py-5 rounded-3xl space-y-5">
         <ActiveBatch />
         <RecentNotification />
