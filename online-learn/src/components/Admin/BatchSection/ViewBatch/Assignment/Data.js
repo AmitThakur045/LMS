5273c6fa34c56dc5e94360a1c038f748b322ecc8
Amit Thakur <at7129652@gmail.com>
2022-05-31 20:09:14 +0530

    started working on assignment section

---
+++ online-learn/src/components/Admin/BatchSection/ViewBatch/Assignment/Data.js
@@ -0,0 +1,168 @@
+export const sampleData = [
+  {
+    courseCode: "CSE101",
+    courseName: "oops",
+    courseImg:
+      "https://static.vecteezy.com/packs/media/components/global/search-explore-nav/img/vectors/term-bg-1-666de2d941529c25aa511dc18d727160.jpg",
+    assignment: [
+      {
+        assignmentCode: "B009CSE10101",
+      },
+      {
+        assignmentCode: "B009CSE10102",
+      },
+      {
+        assignmentCode: "B009CSE10103",
+      },
+    ],
+  },
+  {
+    courseCode: "CSE102",
+    courseName: "JAVA",
+    courseImg:
+      "https://static.vecteezy.com/packs/media/components/global/search-explore-nav/img/vectors/term-bg-1-666de2d941529c25aa511dc18d727160.jpg",
+    assignment: [
+      {
+        assignmentCode: "B009CSE10201",
+      },
+      {
+        assignmentCode: "B009CSE10202",
+      },
+      {
+        assignmentCode: "B009CSE10203",
+      },
+    ],
+  },
+  {
+    courseCode: "CSE103",
+    courseName: "C++",
+    courseImg:
+      "https://static.vecteezy.com/packs/media/components/global/search-explore-nav/img/vectors/term-bg-1-666de2d941529c25aa511dc18d727160.jpg",
+    assignment: [
+      {
+        assignmentCode: "B009CSE10301",
+      },
+      {
+        assignmentCode: "B009CSE10302",
+      },
+      {
+        assignmentCode: "B009CSE10303",
+      },
+    ],
+  },
+];
+
+export const assignment = [
+  {
+    assignmentCode: "B009CSE10101",
+    assignmentTitle: "OOPS",
+    assignmentPdf: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf",
+    student: [
+      {
+        email: "student@gamil.com",
+        image: "https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg",
+        firstName: "Sam",
+        lastName: "Smith",
+        studentAnswer: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf",
+      },
+      {
+        email: "student2@gamil.com",
+        image: "https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg",
+        firstName: "Richy",
+        lastName: "Smith",
+        studentAnswer: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf",
+      },
+      {
+        email: "student3@gamil.com",
+        image: "https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg",
+        firstName: "Jordon",
+        lastName: "Smith",
+        studentAnswer: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf",
+      },
+    ],
+  },
+  {
+    assignmentCode: "B009CSE10102",
+    assignmentTitle: "Inheritance",
+    assignmentPdf: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf",
+    student: [
+      {
+        email: "student@gamil.com",
+        image: "https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg",
+        firstName: "Abhi",
+        lastName: "Smith",
+        studentAnswer: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf",
+      },
+      {
+        email: "student2@gamil.com",
+        image: "https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg",
+        firstName: "Amit",
+        lastName: "Smith",
+        studentAnswer: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf",
+      },
+      {
+        email: "student3@gamil.com",
+        image: "https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg",
+        firstName: "Virat",
+        lastName: "Smith",
+        studentAnswer: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf",
+      },
+    ],
+  },
+  {
+    assignmentCode: "B009CSE10103",
+    assignmentTitle: "Polymorphism",
+    assignmentPdf: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf",
+    student: [
+      {
+        email: "student@gamil.com",
+        image: "https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg",
+        firstName: "Prince",
+        lastName: "Smith",
+        studentAnswer: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf",
+      },
+      {
+        email: "student2@gamil.com",
+        image: "https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg",
+        firstName: "Akassh",
+        lastName: "Smith",
+        studentAnswer: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf",
+      },
+      {
+        email: "student3@gamil.com",
+        image: "https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg",
+        firstName: "Mohit",
+        lastName: "Smith",
+        studentAnswer: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf",
+      },
+    ],
+  },
+  {
+    assignmentCode: "B009CSE10201",
+    assignmentTitle: "Inheritance",
+    assignmentPdf: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf",
+    student: [
+      {
+        email: "student@gamil.com",
+        image: "https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg",
+        firstName: "RAM",
+        lastName: "Smith",
+        studentAnswer: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf",
+      },
+      {
+        email: "student2@gamil.com",
+        image: "https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg",
+        firstName: "RaOne",
+        lastName: "Smith",
+        studentAnswer: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf",
+      },
+      {
+        email: "student3@gamil.com",
+        image: "https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg",
+        firstName: "Spike",
+        lastName: "Smith",
+        studentAnswer: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf",
+      },
+    ],
+  },
+];
