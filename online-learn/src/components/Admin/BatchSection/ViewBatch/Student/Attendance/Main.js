cafef9eda34c19b2b2ec788231a40928cce134a2
Tushar Tomar <tomartushar5@gmail.com>
2022-06-01 19:45:42 +0530

    Added Attendance Section

--- online-learn/src/components/Admin/BatchSection/ViewBatch/Student/Attendance/Main.js
+++ online-learn/src/components/Admin/BatchSection/ViewBatch/Student/Attendance/Main.js
@@ -5,17 +5,36 @@ import {
   getAttendance,
   uploadAttendance,
 } from "../../../../../../Redux/actions/adminActions";
+import { UPLOAD_ATTENDANCE } from "../../../../../../Redux/actionTypes";
+import Spinner from "../../../../../../Utils/Spinner";
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "November",
+  "December",
+];
 const Main = () => {
   const studentData = JSON.parse(localStorage.getItem("students"));
   const batchData = JSON.parse(localStorage.getItem("batch"));
   const dispatch = useDispatch();
-
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState({});
+  const store = useSelector((state) => state);
   const courseCode = JSON.parse(localStorage.getItem("courseCode"));
   const n = 30;
   const [dates, setDates] = useState({});
+  const [attendancess, setAttendancess] = useState([]);
   const [attendanceRecord, setAttendanceRecord] = useState([]);
   useEffect(() => {
     const temp = new Date();
+
     setDates({ year: temp.getFullYear(), month: temp.getMonth() });
     dispatch(
       getAttendance({ batchCode: batchData.batchCode, courseCode: courseCode })
@@ -23,7 +42,6 @@ const Main = () => {
   }, []);
 
   const attendances = useSelector((store) => store.admin.attendance);
-  console.log(attendances);
 
   const markAttendance = (email, date, value) => {
     let data = [...attendanceRecord];
@@ -51,44 +69,91 @@ const Main = () => {
       setAttendanceRecord(data);
     }
   };
-  console.log(attendanceRecord);
 
   const uploadAttendanceRecord = () => {
     dispatch(uploadAttendance(attendanceRecord));
   };
 
+  useEffect(() => {
+    if (store.errors || store.admin.attendanceUploaded) {
+      setLoading(false);
+      if (store.admin.attendanceUploaded) {
+        dispatch({ type: UPLOAD_ATTENDANCE, payload: false });
+        dispatch(
+          getAttendance({
+            batchCode: batchData.batchCode,
+            courseCode: courseCode,
+          })
+        );
+      }
+    } else {
+      setLoading(true);
+    }
+  }, [store.errors, store.admin.attendanceUploaded]);
+
+  const checkDate = (idx, email) => {
+    const date = new Date(dates.year, dates.month, idx);
+    if (attendancess.length === 0) {
+      return "";
+    } else {
+      for (let i = 0; i < attendancess.length; i++) {
+        if (attendancess[i].date.toString() === date.toString()) {
+          for (let j = 0; j < attendancess[i].students.length; j++) {
+            if (attendancess[i].students[j].email === email) {
+              if (attendancess[i].students[j].present === false) {
+                return "A";
+              } else {
+                return "P";
+              }
+            }
+          }
+        }
+      }
+      return "";
+    }
+  };
+
+  useEffect(() => {
+    setAttendancess(attendances);
+  }, [attendances]);
+
   return (
     <div className="mt-4 flex flex-col pb-12 px-12 space-y-6 overflow-y-scroll overflow-x-hidden h-full ">
-      <div className="flex flex-col overflow-x-scroll space-y-3 w-[70rem] h-full">
-        <div className="flex ">
+      <div className="flex flex-col overflow-x-scroll space-y-3 w-[73rem] rounded-lg py-5 px-4 pr-6 h-full bg-[#f7f7f7]">
+        <div className="self-center text-[18px] font-bold text-[#5848a4] mb-4">
+          Attendance Report for {months[dates.month]} of {courseCode}
+        </div>
+        <div className="flex space-x-1">
           {[...Array(n + 1)].map((date, idx) => (
             <div key={idx} className="flex">
               {idx === 0 ? (
-                <div className=" border-2 bg-black text-white w-[15rem] flex items-center px-3 justify-start h-[2.5rem]">
+                <div className="shadow-md bg-white font-semibold text-[#111111] w-[15rem] flex items-center px-3 justify-start h-[2.5rem]">
                   Name
                 </div>
               ) : (
-                <div className="border-2 w-[2.5rem] flex items-center px-3 justify-center h-[2.5rem]">
+                <div className="shadow-md bg-white font-semibold text-[#111111] w-[2.5rem] flex items-center px-3 justify-center h-[2.5rem]">
                   {idx}
                 </div>
               )}
             </div>
           ))}
         </div>
-        <div className="flex flex-col space-y-3">
+        <div className="flex flex-col space-y-3 ">
           {studentData.map((student, index) => (
-            <div key={student.email} className="flex">
+            <div key={student.email} className="flex space-x-1">
               {[...Array(n + 1)].map((date, idx) => (
-                <div key={idx} className="flex">
+                <div key={idx} className="flex ">
                   {idx === 0 ? (
-                    <div className=" border-2 bg-black text-white w-[15rem] flex items-center px-3 justify-start h-[2.5rem]">
+                    <div className="shadow-md bg-white font-semibold text-[#5848e4] w-[15rem] flex items-center px-3 justify-start h-[2.5rem]">
                       {student.firstName} {student.lastName}
                     </div>
                   ) : (
                     <input
+                      className="shadow-md bg-white font-semibold text-[#5848e4] w-[2.5rem] flex items-center px-3 justify-center h-[2.5rem]"
                       dt={new Date(dates.year, dates.month, idx)}
                       em={student.email}
                       maxLength={1}
+                      placeholder={checkDate(idx, student.email)}
                       autoCapitalize
                       key={idx}
                       onChange={(e) =>
@@ -99,7 +164,6 @@ const Main = () => {
                         )
                       }
                       type="text"
-                      className="border-2 w-[2.5rem] flex items-center px-3 justify-center h-[2.5rem]"
                     />
                   )}
                 </div>
@@ -114,6 +178,7 @@ const Main = () => {
         variant="contained">
         Mark Attendance
       </Button>
+      {loading && <Spinner message="Uploading Attendance" />}
     </div>
   );
 };
