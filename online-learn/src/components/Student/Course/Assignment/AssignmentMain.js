1fae51c3437034eb1939921dbeea66fe87aed04b
Amit Thakur <at7129652@gmail.com>
2022-06-17 21:58:30 +0530

    added responsiveness to assignment

--- online-learn/src/components/Student/Course/Assignment/AssignmentMain.js
+++ online-learn/src/components/Student/Course/Assignment/AssignmentMain.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import PeopleIcon from "@mui/icons-material/People";
 import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
 import FormatListNumberedIcon from "@mui/icons-material/FormatListNumbered";
 import { NavLink } from "react-router-dom";
-
+import Button from "@mui/material/Button";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 import { useDispatch, useSelector } from "react-redux";
 import PdfViewer from "./PdfViewer";
 import { getAssignmentByBatchCode } from "../../../../Redux/actions/studentActions";
@@ -37,12 +38,15 @@ const sampleData = [
 
 const AssignmentMain = () => {
   const dispatch = useDispatch();
+  const inputRef = useRef(null);
   const learner = JSON.parse(localStorage.getItem("learner"));
   const assignment = useSelector((state) => state.student.assignment);
   const [allAssignmet, setAllAssignment] = useState([]);
   const [selectedPdf, setSelectedPdf] = useState("");
   const batch = useSelector((state) => state.admin.batch);
   const [batchData, setBatchData] = useState({});
+  const [value, setValue] = useState();
+  const [isSelected, setIsSelected] = useState(false);
   const [index, setIndex] = useState(0);
 
   useEffect(() => {
@@ -69,6 +73,18 @@ const AssignmentMain = () => {
     }
   }, [batch]);
 
+  const changeHandler = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(file);
+      fileReader.onloadend = (e) => {
+        setValue(fileReader.result);
+      };
+    }
+    setIsSelected(true);
+  };
+
   const pdfHandler = (data) => {
     const raw = window.atob(JSON.stringify(data));
     const rawLength = raw.length;
@@ -87,17 +103,18 @@ const AssignmentMain = () => {
   return (
     <>
       {Object.keys(batchData).length !== 0 && (
-        <div className="flex h-full">
-          <div className="flex-[0.4] bg-[#EDF0F5] rounded-bl-2xl">
+        <div className="flex lg:flex-row flex-col w-full lg:h-[80vh]">
+          <div className="lg:flex-[0.4] flex-col w-full bg-[#EDF0F5] rounded-bl-2xl lg:h-full pb-5">
             <h1 className="ml-6 mt-7 font-semibold text-2xl">Assignment</h1>
-            <div className="space-y-4 mt-4 overflow-y-auto h-[33rem]">
+            <div className="flex-col space-y-4 mt-4 lg:h-[60vh] h-[50vh] overflow-y-auto">
               {allAssignmet.map((data, i) => (
                 <div
-                  className="bg-[#127FED] h-[10rem] mx-6 rounded-xl px-4 py-4 text-white flex flex-col justify-between hover:cursor-pointer"
+                  className="bg-[#127FED] h-[10rem] mx-6 rounded-xl px-4 py-4 text-white flex flex-col justify-between overflow-auto hover:cursor-pointer"
                   key={i}
                   onClick={() => {
                     setSelectedPdf(data.assignmentPdf);
-                  }}>
+                  }}
+                >
                   <h3>Assignment code: {data.assignmentCode}</h3>
                   <div className="flex space-x-2">
                     <h1 className="text-[21px] flex justify-center items-center">
@@ -105,16 +122,38 @@ const AssignmentMain = () => {
                     </h1>
                     <h1>{data.assignmentDescription}</h1>
                   </div>
-                  <div className="">
+                  <div className="flex space-x-2">
                     <span className="bg-[#6EAEE9] flex items-center justify-center w-[5rem] rounded-2xl h-[2rem] ">
                       {data.courseCode}
                     </span>
+                    <Button
+                      type="button"
+                      style={{
+                        backgroundColor: "#6EAEE9",
+                        borderRadius: "20px",
+                        padding: "0",
+                      }}
+                      disabled={isSelected}
+                      onClick={() => inputRef.current.click()}
+                    >
+                      <div className="flex text-white text-center px-2 py-0 space-x-1">
+                        <div>{isSelected ? `Uploaded` : `Upload`}</div>
+                        <CloudUploadIcon />
+                      </div>
+                      <input
+                        type="file"
+                        ref={inputRef}
+                        name="file"
+                        onChange={changeHandler}
+                        className="hidden"
+                      />
+                    </Button>
                   </div>
                 </div>
               ))}
             </div>
           </div>
-          <div className="flex-[0.6] bg-[#F9F9F9] overflow-y-auto">
+          <div className="lg:flex-[0.6] bg-[#F9F9F9] w-full overflow-y-auto ">
             <PdfViewer pdf={selectedPdf} />
           </div>
         </div>
