1b5e61a809d9a4426d5191fba159943cba36b4f8
Amit Thakur <at7129652@gmail.com>
2022-06-20 20:07:45 +0530

    made community responsive

--- online-learn/src/components/Student/Community/Community.js
+++ online-learn/src/components/Student/Community/Community.js
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import Button from "@mui/material/Button";
 import ForumSharpIcon from "@mui/icons-material/ForumSharp";
 import ArrowRightSharpIcon from "@mui/icons-material/ArrowRightSharp";
 import ArrowDropDownSharpIcon from "@mui/icons-material/ArrowDropDownSharp";
 import HomeSidebar from "../HomeSidebar";
+import HomeDrawer from "../HomeDrawer";
+import MenuIcon from "@mui/icons-material/Menu";
+import CancelIcon from "@mui/icons-material/Cancel";
 
 const isNotActiveStyle = "text-[#555555] flex flex-col items-center";
 const isActiveStyle =
@@ -68,21 +70,47 @@ const colors = ["red", "blue", "green", "blue", "orange"];
 const Community = () => {
   const navigate = useNavigate();
   const [isActive, setIsActive] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  //choose the screen size
+  const handleResize = () => {
+    if (window.innerWidth < 678) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  };
+
+  // create an event listener
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+  }, [window.innerWidth]);
 
   const logout = () => {
     navigate("/login/adminLogin");
   };
 
   return (
-    <div className="bg-[#1a1a1a] w-screen h-screen flex overflow-hidden">
+    <div className="bg-[#1a1a1a] w-full h-screen flex overflow-hidden">
       <HomeSidebar />
-      <div className=" bg-white flex-[0.93] my-4 rounded-2xl mr-4 px-[3.1rem] pt-[3rem]">
+      <div className="bg-white w-full md:flex flex-col my-4 rounded-2xl sm:mr-4 sm:mx-0 mx-2 px-[3.1rem] pt-[3rem] overflow-auto">
+        {isMobile && (
+          <div className="absolute h-[5rem] justify-end text-black right-4 top-5">
+            {isOpen ? (
+              <CancelIcon onClick={() => setIsOpen(false)} />
+            ) : (
+              <MenuIcon onClick={() => setIsOpen(true)} />
+            )}
+          </div>
+        )}
+        {isOpen && <HomeDrawer isOpen={isOpen} setIsOpen={setIsOpen} />}
         <div>
           <p className="text-4xl font-bold">Community</p>
         </div>
-        <div className="flex flex-row space-x-8">
-          <div className="flex flex-col flex-[0.68]">
-            <div className="w-auto h-40 rounded-3xl shadow-lg p-8 mt-4 space-y-2">
+        <div className="flex md:flex-row flex-col space-x-8 space-y-6">
+          <div className="flex flex-col flex-[0.68] overflow-auto">
+            <div className="w-auto rounded-3xl shadow-lg p-8 mt-4 space-y-2">
               <div>
                 <div className="flex flex-row space-x-20 ">
                   <div className="flex">
@@ -90,7 +118,7 @@ const Community = () => {
                       Welcome to LearnVerse Community
                     </p>
                   </div>
-                  <div className="space">
+                  <div className="md:p-2 sm:p-1 p-[0.1rem]">
                     <Button
                       style={{
                         borderRadius: 14,
@@ -99,9 +127,9 @@ const Community = () => {
                         fontSize: "15px",
                         fontWeight: "700",
                         color: "#fff",
-                        height: "38px",
                       }}
-                      variant="contained">
+                      variant="contained"
+                    >
                       New Thread
                     </Button>
                   </div>
@@ -113,52 +141,52 @@ const Community = () => {
                 <hr className="border-black mt-4"></hr>
               </div>
 
-              <div className="border-white py-10"></div>
+              {/* <div className="border-white md:py-20 py-10"></div> */}
             </div>
 
-            <div className="flex-col p-0 m-0 overflow-y-auto h-[26rem] mt-6">
+            <div className="p-0 m-0 overflow-y-auto h-[20rem] mt-6">
               {dummyData.map((data) => (
-                <div>
-                  <div className="flex flex-col space-y-2 w-auto h-fit rounded-3xl shadow-lg p-8 mt-4">
-                    <div className="border-2 border-black rounded-xl w-fit px-1">
-                      {data.title}
-                    </div>
-                    <div className="">
-                      <p className="text-2xl font-bold pt-1 ">
-                        {data.description}
+                <div className="flex flex-col space-y-2 w-auto h-fit rounded-3xl shadow-lg p-8 mt-4">
+                  <div className="border-2 border-black rounded-xl w-fit px-1">
+                    {data.title}
+                  </div>
+                  <div className="">
+                    <p className="text-2xl font-bold pt-1 ">
+                      {data.description}
+                    </p>
+                  </div>
+                  <hr className="border-black mt-4"></hr>
+                  <div className="flex flex-row justify-between">
+                    <div className="flex space-x-6">
+                      <p className="text-sm font-base pt-1 ">
+                        Replies: {data.replies}
                       </p>
+                      <Button
+                        style={{
+                          borderRadius: 14,
+                          backgroundColor: "#FF9C72",
+                          padding: "9px 21px",
+                          fontSize: "15px",
+                          fontWeight: "700",
+                          color: "#fff",
+                          height: "38px",
+                        }}
+                        variant="contained"
+                      >
+                        {data.answer}
+                      </Button>
                     </div>
-                    <hr className="border-black mt-4"></hr>
-                    <div className="flex flex-row justify-between">
-                      <div className="flex space-x-6">
-                        <p className="text-sm font-base pt-1 ">
-                          Replies: {data.replies}
-                        </p>
-                        <Button
-                          style={{
-                            borderRadius: 14,
-                            backgroundColor: "#FF9C72",
-                            padding: "9px 21px",
-                            fontSize: "15px",
-                            fontWeight: "700",
-                            color: "#fff",
-                            height: "38px",
-                          }}
-                          variant="contained">
-                          {data.answer}
-                        </Button>
+                    <div className="flex space-x-3">
+                      <div className="flex flex-col">
+                        <p>{data.user}</p>
+                        <p className="justify-self-end">{data.repliedAt}</p>
                       </div>
-                      <div className="flex space-x-3">
-                        <div className="flex flex-col">
-                          <p>{data.user}</p>
-                          <p className="justify-self-end">{data.repliedAt}</p>
-                        </div>
-                        <div
-                          className={`border-2 h-[50px] w-[50px] rounded-full flex items-center justify-center font-bold bg-${
-                            colors[Math.floor(Math.random() * colors.length)]
-                          }-500`}>
-                          {data.avatar}
-                        </div>
+                      <div
+                        className={`border-2 h-[50px] w-[50px] rounded-full flex items-center justify-center font-bold bg-${
+                          colors[Math.floor(Math.random() * colors.length)]
+                        }-500`}
+                      >
+                        {data.avatar}
                       </div>
                     </div>
                   </div>
@@ -167,7 +195,7 @@ const Community = () => {
             </div>
           </div>
 
-          <div className="flex flex-col flex-[0.32] mr-3 space-y-3 rounded-3xl shadow-lg h-fit p-3">
+          <div className="flex flex-col flex-[0.32] mr-3 w-full md:w-auto space-y-3 rounded-3xl shadow-lg h-fit p-3">
             <div className="flex flex-row space-x-5 ">
               <ForumSharpIcon className="mt-2" fontSize="large" alt="" />
               <p className="text-4xl font-bold">Forums</p>
