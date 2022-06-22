75fff389a681850896ae8dd251db2271bc577e89
Amit Thakur <at7129652@gmail.com>
2022-06-22 16:27:58 +0530

    course ui error

--- online-learn/src/components/Student/HomeSidebar.js
+++ online-learn/src/components/Student/HomeSidebar.js
@@ -52,7 +52,7 @@ const HomeSidebar = () => {
 
   return (
     <div className="hidden sm:block">
-      <div className="flex-[0.07] flex flex-col h-full lg:my-4 my-2 justify-between py-5">
+      <div className="flex-[0.07] flex flex-col h-full justify-between py-5">
         <div className="flex justify-center">
           <img className="lg:h-14 lg:w-14 h-12 w-12" src={logo} alt="" />
         </div>
