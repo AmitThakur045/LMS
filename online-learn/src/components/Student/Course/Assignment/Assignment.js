d52d2cdef196e790c09526c195551d8d15d5ff05
Amit Thakur <at7129652@gmail.com>
2022-06-22 20:43:36 +0530

    added loader

--- online-learn/src/components/Student/Course/Assignment/Assignment.js
+++ online-learn/src/components/Student/Course/Assignment/Assignment.js
@@ -1,22 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import CourseHeader from "../CourseHeader";
 import CourseSidebar from "../CourseSidebar";
 import AssignmentMain from "./AssignmentMain";
+import Loader from "../../../../Utils/Loader";
 
 const Course = () => {
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("learner")));
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1200);
+  }, []);
 
   return (
-    <div className="bg-[#1a1a1a] w-full sm:w-screen h-screen flex overflow-hidden">
-      <CourseSidebar />
-      {user !== null && (
-        <div className="bg-white flex my-4 w-full rounded-2xl sm:mr-4 sm:mx-0 mx-2 flex-col lg:overflow-hidden overflow-auto">
-          <CourseHeader />
-          <AssignmentMain />
+    <>
+      {isLoading ? (
+        <div className="flex items-center justify-center h-screen w-full">
+          <Loader isLoading={isLoading} />
+        </div>
+      ) : (
+        <div className="bg-[#1a1a1a] w-full sm:w-screen h-screen flex overflow-hidden">
+          <CourseSidebar />
+          {user !== null && (
+            <div className="bg-white flex my-4 w-full rounded-2xl sm:mr-4 sm:mx-0 mx-2 flex-col lg:overflow-hidden overflow-auto">
+              <CourseHeader />
+              <AssignmentMain />
+            </div>
+          )}
         </div>
       )}
-    </div>
+    </>
   );
 };
 
--- online-learn/src/components/Student/Home/Home.js
+++ online-learn/src/components/Student/Home/Home.js
@@ -2,14 +2,31 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import HomeSidebar from "../HomeSidebar";
 import Main from "./Main";
+import Loader from "../../../Utils/Loader";
 
 const Home = () => {
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("learner")));
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2500);
+  }, []);
+
   return (
-    <div className="bg-[#1a1a1a] w-full h-screen flex overflow-hidden">
-      <HomeSidebar />
-      {user !== null && <Main />}
-    </div>
+    <>
+      {isLoading ? (
+        <div className="flex items-center justify-center h-screen w-full">
+          <Loader isLoading={isLoading} />
+        </div>
+      ) : (
+        <div className="bg-[#1a1a1a] w-full h-screen flex overflow-hidden">
+          <HomeSidebar />
+          {user !== null && <Main />}
+        </div>
+      )}
+    </>
   );
 };
 
