43e69c250e911463b083d10555b9fc250fef3bc1
Tushar Tomar <tomartushar5@gmail.com>
2022-06-22 16:32:32 +0530

    UI

--- online-learn/src/components/Student/Home/Home.js
+++ online-learn/src/components/Student/Home/Home.js
@@ -6,7 +6,7 @@ import Main from "./Main";
 const Home = () => {
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("learner")));
   return (
-    <div className="bg-[#1a1a1a] w-full h-screen flex">
+    <div className="bg-[#1a1a1a] w-full h-screen flex overflow-hidden">
       <HomeSidebar />
       {user !== null && <Main />}
     </div>
