299a9df1d24f0e5bd0c72632f6dfe5df3a5fd805
Amit Thakur <at7129652@gmail.com>
2022-06-26 21:19:17 +0530

    view admin responsiveness

--- online-learn/src/components/Admin/AdminSection/ViewAdmin/Main.js
+++ online-learn/src/components/Admin/AdminSection/ViewAdmin/Main.js
@@ -29,12 +29,12 @@ const Main = () => {
   }, []);
 
   return (
-    <div className="flex overflow-hidden h-full space-x-5 px-12 mb-5">
-      <div className="w-[80%] rounded-3xl bg-[#FAFBFF] px-10 py-5 flex flex-col space-y-4">
+    <div className="flex lg:flex-row flex-col overflow-y-auto h-full space-x-5 lg:px-12 px-2 mb-5">
+      <div className="lg:w-[80%] w-full rounded-3xl bg-[#FAFBFF] lg:px-10 px-2 py-5 flex flex-col space-y-4">
         <p className="text-[#8d91b1]">View Admin</p>
-        <div className="flex space-x-16">
-          <div className="w-[40%] flex items-center justify-center">
-            <div className="w-[250px] h-[227px] bg-white border-[1px] border-[#CBCBCB] flex flex-col items-center justify-center">
+        <div className="flex flex-col w-full sm:flex-row sm:items-start items-center lg:space-x-16 space-x-4 space-y-6 sm:space-y-0">
+          <div className="w-[40%] flex items-start justify-center">
+            <div className="lg:w-[250px] w-[10rem] lg:h-[227px] h-[10rem] bg-white border-[1px] border-[#CBCBCB] flex flex-col items-center justify-center">
               <img
                 src={admin.avatar}
                 className="w-full h-full object-cover"
@@ -42,8 +42,8 @@ const Main = () => {
               />
             </div>
           </div>
-          <div className="flex flex-col w-[60%] space-y-6">
-            <div className="flex justify-between ">
+          <div className="flex flex-col sm:w-[60%] space-y-6">
+            <div className="flex flex-col lg:flex-row justify-between space-y-6 lg:space-y-0 lg:space-x-2 ">
               <TextField
                 aria-disabled
                 type="text"
@@ -100,7 +100,8 @@ const Main = () => {
                   labelId="demo-simple-select-label"
                   id="demo-simple-select"
                   value={admin.sub}
-                  label="Sub Admin">
+                  label="Sub Admin"
+                >
                   <MenuItem value="true">Yes</MenuItem>
                   <MenuItem value="false">No</MenuItem>
                 </Select>
@@ -125,8 +126,7 @@ const Main = () => {
           <p className="text-red-500 flex self-center">{error.emailError}</p>
         )}
       </div>
-
-      <div className="bg-[#FAFBFF] w-[20%] flex flex-col px-5 py-5 rounded-3xl space-y-5">
+      <div className="bg-[#FAFBFF] lg:w-[20%] flex lg:flex-col flex-row lg:items-center items-start lg:pl-5 py-5 rounded-3xl lg:space-y-5 space-x-3 lg:space-x-0">
         <ActiveBatch />
         <RecentNotification />
       </div>
