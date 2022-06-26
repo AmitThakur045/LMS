ce381180819ff094d3bb8bfe4d1ba20621c0678c
Amit Thakur <at7129652@gmail.com>
2022-06-26 20:15:29 +0530

    dashboard responsive

--- online-learn/src/components/Admin/DashboardSection/Dashboard.js
+++ online-learn/src/components/Admin/DashboardSection/Dashboard.js
@@ -22,10 +22,12 @@ const Dashboard = () => {
           <Loader isLoading={isLoading} />
         </div>
       ) : (
-        <div className="h-screen w-full bg-[#ffffff] flex overflow-hidden">
-          <Sidebar />
+        <div className="flex h-screen w-full bg-[#ffffff] overflow-hidden">
+          <div className="lg:flex-[0.15] flex-[0.25] h-screen">
+            <Sidebar />
+          </div>
           {user !== null && (
-            <div className="flex flex-col  w-full">
+            <div className="lg:flex-[0.85] flex-[0.75] flex flex-col">
               <Header type="Dashboard" />
               <Main />
             </div>
