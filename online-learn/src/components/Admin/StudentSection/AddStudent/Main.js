de70b4941cccbabe06ad7205b29787c4e4e5d70b
Amit Thakur <at7129652@gmail.com>
2022-06-27 19:08:51 +0530

    responsive student section

--- online-learn/src/components/Admin/StudentSection/AddStudent/Main.js
+++ online-learn/src/components/Admin/StudentSection/AddStudent/Main.js
@@ -124,14 +124,15 @@ const Main = () => {
   };
 
   return (
-    <div className="flex overflow-hidden h-full space-x-5 px-12 mb-5">
+    <div className="flex lg:flex-row flex-col overflow-y-auto h-full space-x-5 lg:px-12 px-2 mb-5">
       <form
         onSubmit={handleSubmit}
-        className="w-[80%] rounded-3xl bg-[#FAFBFF] px-10 py-5 flex flex-col space-y-4">
+        className="lg:w-[80%] w-full rounded-3xl bg-[#FAFBFF] lg:px-10 px-2 py-5 flex flex-col space-y-4"
+      >
         <p className="text-[#8d91b1]">Add Student</p>
-        <div className="flex space-x-16">
-          <div className="w-[40%] flex items-center justify-center">
-            <div className="w-[250px] h-[227px] bg-white border-[1px] border-[#CBCBCB] flex flex-col items-center justify-center">
+        <div className="flex flex-col w-full sm:flex-row sm:items-start items-center lg:space-x-16 space-x-4 space-y-6 sm:space-y-0">
+          <div className="w-[40%] flex items-start justify-center">
+            <div className="lg:w-[250px] w-[10rem] lg:h-[227px] h-[10rem] bg-white border-[1px] border-[#CBCBCB] flex flex-col items-center justify-center">
               {values.avatar !== "" ? (
                 <img
                   src={values.avatar}
@@ -142,12 +143,13 @@ const Main = () => {
                 <div className="">
                   <label
                     className="flex items-center justify-center flex-col space-y-3"
-                    for="image">
+                    for="image"
+                  >
                     <MdOutlineFileUpload
                       className="w-14 rounded-full h-14 bg-[#d8d8d8] cursor-pointer"
                       fontSize={35}
                     />
-                    <p>Upload Profile Picture</p>
+                    <p className="w-full text-center">Upload Profile Picture</p>
                   </label>
                   <input
                     id="image"
@@ -161,15 +163,15 @@ const Main = () => {
               )}
             </div>
           </div>
-          <div className="flex flex-col w-[60%] space-y-6">
-            <div className="flex justify-between ">
+          <div className="flex flex-col sm:w-[60%] w-full space-y-6">
+            <div className="flex flex-col lg:flex-row justify-between space-y-6 lg:space-y-0 lg:space-x-2 ">
               <TextField
                 required
                 type="text"
                 id="outlined-basic"
                 label="First Name"
                 variant="outlined"
-                className="bg-white"
+                className="bg-white w-full"
                 value={values.firstName}
                 onChange={(e) =>
                   setValues({ ...values, firstName: e.target.value })
@@ -181,21 +183,21 @@ const Main = () => {
                 id="outlined-basic"
                 label="Last Name"
                 variant="outlined"
-                className="bg-white"
+                className="bg-white w-full"
                 value={values.lastName}
                 onChange={(e) =>
                   setValues({ ...values, lastName: e.target.value })
                 }
               />
             </div>
-            <div className="flex space-x-8">
+            <div className="flex flex-col lg:flex-row justify-between space-y-6 lg:space-y-0 lg:space-x-2 ">
               <TextField
                 required
                 type="email"
                 id="outlined-basic"
                 label="Email"
                 variant="outlined"
-                className="bg-white"
+                className="bg-white w-full"
                 value={values.email}
                 onChange={(e) =>
                   setValues({ ...values, email: e.target.value })
@@ -206,18 +208,18 @@ const Main = () => {
                 type="date"
                 id="outlined-basic"
                 variant="outlined"
-                className="bg-white "
+                className="bg-white w-full"
                 value={values.dob}
                 onChange={(e) => setValues({ ...values, dob: e.target.value })}
               />
             </div>
-            <div className="flex space-x-8 ">
+            <div className="flex">
               <TextField
                 type="number"
                 id="outlined-basic"
                 label="Contact Number"
                 variant="outlined"
-                className="bg-white w-[60%]"
+                className="bg-white w-full sm:w-[50%]"
                 value={values.contactNumber}
                 onChange={(e) =>
                   setValues({ ...values, contactNumber: e.target.value })
@@ -228,7 +230,8 @@ const Main = () => {
         </div>
         <button
           type="submit"
-          className="self-end bg-[#FB6C3A] h-[3rem] text-white w-[10rem] rounded-md text-[17px] hover:bg-[#e54e17] transition-all duration-150">
+          className="self-end bg-[#FB6C3A] h-[3rem] text-white w-[10rem] rounded-md text-[17px] hover:bg-[#e54e17] transition-all duration-150"
+        >
           Submit
         </button>
         {loading && <Spinner message="Adding Student" />}
@@ -237,7 +240,7 @@ const Main = () => {
         )}
       </form>
 
-      <div className="bg-[#FAFBFF] w-[20%] flex flex-col px-5 py-5 rounded-3xl space-y-5">
+      <div className="bg-[#FAFBFF] lg:w-[20%] flex lg:flex-col flex-row lg:items-center items-start lg:pl-5 py-5 rounded-3xl lg:space-y-5 space-x-3 lg:space-x-0">
         <ActiveBatch />
         <RecentNotification />
       </div>
