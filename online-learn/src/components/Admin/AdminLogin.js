8261601aecad668e510b3c1c58e6fbab8e762ac1
Amit Thakur <at7129652@gmail.com>
2022-07-07 20:49:54 +0530

    responsive admin login section

--- online-learn/src/components/Admin/AdminLogin.js
+++ online-learn/src/components/Admin/AdminLogin.js
@@ -44,12 +44,13 @@ const AdminLogin = () => {
     }
   }, [store.errors]);
   return (
-    <div className="bg-[#04bd7d] h-screen w-screen flex items-center justify-center">
-      <div className="grid grid-cols-2">
+    <div className="bg-[#04bd7d] h-screen w-full flex items-center justify-center">
+      <div className="grid lg:grid-cols-2 gird-cols-1">
         <div
-          className={`h-96 w-96 bg-white flex items-center justify-center ${
+          className={`h-96 w-96 hidden bg-white lg:flex items-center justify-center ${
             translate ? "translate-x-[12rem]" : ""
-          }  duration-1000 transition-all rounded-3xl shadow-2xl`}>
+          }  duration-1000 transition-all rounded-3xl shadow-2xl`}
+        >
           <h1 className="text-[3rem]  font-bold text-center">
             Admin
             <br />
@@ -60,32 +61,33 @@ const AdminLogin = () => {
           onSubmit={login}
           className={`${
             loading ? "h-[27rem]" : "h-96"
-          } w-96 bg-[#2c2f35] flex flex-col items-center justify-center ${
-            translate ? "-translate-x-[12rem]" : ""
-          }  duration-1000 transition-all space-y-6 rounded-3xl shadow-2xl`}>
+          } sm:w-96 w-[80vw] px-3 bg-[#2c2f35] flex flex-col items-center justify-center ${
+            translate ? "lg:-translate-x-[12rem]" : ""
+          }  duration-1000 transition-all space-y-6 rounded-3xl shadow-2xl`}
+        >
           <h1 className="text-white text-3xl font-semibold">Admin</h1>
-          <div className="space-y-1">
-            <p className="text-[#515966] font-bold text-sm">Email</p>
-            <div className="bg-[#515966] rounded-lg w-[14rem] flex  items-center">
+          <div className="space-y-1 w-full flex flex-col items-center">
+            <p className="text-[#515966] font-bold text-sm text-left sm:w-[14rem] w-full">Email</p>
+            <div className="bg-[#515966] rounded-lg sm:w-[14rem] w-full flex  items-center">
               <input
                 onChange={(e) => setEmail(e.target.value)}
                 value={email}
                 type="text"
                 required
-                className="bg-[#515966] text-white px-2 outline-none py-2 rounded-lg placeholder:text-sm"
+                className="bg-[#515966] text-white px-2 outline-none py-2 w-full rounded-lg placeholder:text-sm"
                 placeholder="Username"
               />
             </div>
           </div>
-          <div className="space-y-1">
-            <p className="text-[#515966] font-bold text-sm">Password</p>
-            <div className="bg-[#515966] rounded-lg px-2 flex  items-center">
+          <div className="space-y-1 w-full flex flex-col items-center">
+            <p className="text-[#515966] font-bold text-sm text-left sm:w-[14rem] w-full">Password</p>
+            <div className="bg-[#515966] rounded-lg px-2 flex sm:w-[14rem] w-full  items-center">
               <input
                 onChange={(e) => setPassword(e.target.value)}
                 value={password}
                 required
                 type={showPassword ? "text" : "password"}
-                className=" bg-[#515966] text-white rounded-lg outline-none py-2  placeholder:text-sm"
+                className=" bg-[#515966] text-white rounded-lg outline-none py-2 w-full placeholder:text-sm"
                 placeholder="Password"
               />
               {showPassword ? (
@@ -103,7 +105,8 @@ const AdminLogin = () => {
           </div>
           <button
             type="submit"
-            className="w-32 hover:scale-105 transition-all duration-150 rounded-lg flex items-center justify-center text-white text-base py-1 bg-[#04bd7d]">
+            className="w-32 hover:scale-105 transition-all duration-150 rounded-lg flex items-center justify-center text-white text-base py-1 bg-[#04bd7d]"
+          >
             Login
           </button>
           {loading && (
